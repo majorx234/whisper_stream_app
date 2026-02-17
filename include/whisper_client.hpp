2c6f44964c8051cf17219a0a827531a0dc958675
MajorX234 <majorx234@googlemail.com>
2026-02-17 23:59:48 +0100

    Add skeleton for client lib

---
+++ include/whisper_client.hpp
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the whisper_stream_app distribution (https://github.com/majorx234/whisper_stream_app ).
+ * Copyright (c) 2026 Majorx234
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef WHISPER_CLIENT_HPP_
+#define WHISPER_CLIENT_HPP_
+
+class WhisperClient{
+
+};
+
+#endif //WHISPER_CLIENT_HPP_
